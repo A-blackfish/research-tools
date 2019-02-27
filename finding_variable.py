46f43e085a3ffffde90ee4cf76311ff93de40c1f
Blackfish <30684435+A-blackfish@users.noreply.github.com>
2019-02-27 16:16:54 +0100

    Create finding_variable.py

---
+++ finding_variable.py
@@ -0,0 +1,16 @@
+s1 = 8
+s2 = 10
+s3 = 12
+
+value = ['s1', 's2', 's3']
+i=1
+name='{}{}'.format('s', i)
+x=eval(name)
+print(x)
+
+# output is 8
+# this script can help you finding variable according to its name
+
+# a = 's'+1
+# print 's1:', s1
+# print 'a:', a
